5375ec3e14e9bd2393497db40d17324a1617c0be
Nikita Roy <nikitaroy@Nikitas-Laptop.local>
2025-03-14 00:05:38 +0530

    Added meta tags in index.html Updated App.js Added necessary folders and files Added pages map.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 
 import './App.css';
+import {Route,Routes} from "react-router-dom";
+import pages from "./utils/pages";
+import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+import Menu from "./pages/Menu/Menu";
+import Reservations from "./pages/Reservations/Reservations";
+import Orders from "./pages/Orders/Orders";
+import About from "./pages/About/About";
+import Layout from "./layouts/Layout/Layout";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Capstone project</h1>
+    <div data-testid="app-component">
+      <Layout>
+        <Routes>
+          <Route path ={pages.get("home").path} element={<Home />} />
+          <Route path ={pages.get("about").path} element={<About />} />
+          <Route path={pages.get("menu").path} element={<Menu />} />
+          <Route path={pages.get("reservations").path} element={<Reservations />} />
+          <Route path={pages.get("orders").path} element={<Orders />} />
+          <Route path={pages.get("login").path} element={<Login />} />
+          <Route path="*" element={<Home />} />
+        </Routes>
+      </Layout>
     </div>
   );
 }
---
+++ src/utils/pages.js
@@ -0,0 +1,8 @@
+const pages = new Map();
+pages.set('home', { name: 'Home', path: '/', anchorable: true})
+pages.set('about', { name: 'About', path: '/about', anchorable: true});
+pages.set('menu', { name: 'Menu', path: '/menu', anchorable: true});
+pages.set('reservations', { name: 'Reservations', path: '/reservations', anchorable: true});
+pages.set('confirmedBooking', { name: 'Confirmed Booking', path: '/confimed-booking', anchorable: true});
+pages.set('orders', { name: 'Order Online', path: '/orders',anchorable: true});
+pages.set('login', {name: 'Login', path: '/login', anchorable: true});
